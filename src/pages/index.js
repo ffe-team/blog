47684e336d48ec52bb381e79195b1895122fc1a4
blackcater <blackcater2015@gmail.com>
2017-07-25 23:48:45 +0800

    init project
    using gatsby

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    )
+  }
+}
